0432e3c27fff3e604a08ebc5f1cd7a0af6a1bf4f
Iqbal Nurjadin <iqbalnurjadin@berkeley.edu>
2020-08-18 20:38:03 -0500

    Add OpenWeather API and hide key

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import * as config from "/Users/iqbalnurjadin/Desktop/cs_projects/weather-app/config.js"
 import { parse } from "https://deno.land/std@0.61.0/flags/mod.ts";
 
 const args = parse(Deno.args);
@@ -5,5 +6,11 @@ if (args.city == undefined) {
     console.error("No city supplied");
     Deno.exit();
 }
+// console.log(args);
 
-console.log(args);
+const apiKey = config.myKey;
+
+const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${args.city}&units=metric&appid=${apiKey}`);
+const data = await res.json();
+
+console.log(data);
